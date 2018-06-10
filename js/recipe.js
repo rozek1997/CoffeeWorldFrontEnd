42f01ee35ee359efe68e2f04300c8cdafb5b9b3c
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2018-06-10 22:58:42 +0200

    Ready for deployment

--- js/recipe.js
+++ js/recipe.js
@@ -1,5 +1,5 @@
 function getJson() {
-    var dataFile = 'http://localhost:8080/coffee/';
+    var dataFile = 'http://localhost:8080/recipes/';
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
